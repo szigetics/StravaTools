4addfbf00c48a1bc6b361ba5b7de588543983a44
Csaba Szigeti <szigetics@gmail.com>
2020-05-16 15:50:07 +0200

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,6 +2,10 @@
 
 set -eo pipefail #Stop execution in case if any commands fail
 
+#setup (fake) strava API client id and client secret
+touch strava.clientID
+touch strava.clientSecret
+
 xcodebuild -project StravaTools.xcodeproj \
 -scheme StravaTools \
 -destination platform=iOS\ Simulator,OS=13.4.1,name=iPhone\ 11 \
