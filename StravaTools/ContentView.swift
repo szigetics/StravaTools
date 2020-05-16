56e646a48ad2d85bf9c7e475f57b6fcf3a73fc56
Csaba Szigeti <csaba.szigeti@pix4d.com>
2020-05-16 10:30:06 +0200

    Adding iOS Single View app project freshly generated by Xcode

---
+++ StravaTools/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StravaTools
+//
+//  Created by Csaba Szigeti on 2020. 05. 16..
+//  Copyright © 2020. Csaba Szigeti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
