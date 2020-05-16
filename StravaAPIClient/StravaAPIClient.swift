6851d3f48220186ec82f0c9a223eaf11e9b2eecf
Csaba Szigeti <csaba.szigeti@pix4d.com>
2020-05-16 10:34:04 +0200

    Skeleton

---
+++ StravaAPIClient/StravaAPIClient.swift
@@ -0,0 +1,23 @@
+//
+//  StravaAPIClient.swift
+//  StravaTools
+//
+//  Created by Csaba Szigeti on 2020. 05. 16..
+//  Copyright © 2020. Csaba Szigeti. All rights reserved.
+//
+
+import Foundation
+
+class StravaAPIClient {
+    func authenticate() {
+        //TODO
+    }
+    
+    func currentAthlete() {
+        //TODO
+    }
+    
+    func listActivities() {
+        //TODO
+    }
+}
