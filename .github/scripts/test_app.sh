7842a890dc7ab70d87bb3c21cf5726018c28bb90
Csaba Szigeti <szigetics@gmail.com>
2020-05-16 15:44:41 +0200

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,6 +5,7 @@ set -eo pipefail #Stop execution in case if any commands fail
 xcodebuild -project StravaTools.xcodeproj \
 -scheme StravaTools
 -destination platform=iOS\ Simulator,OS=13.4.1,name=iPhone\ 11 \
+CODE_SIGN_STYLE="Manual" \
 CODE_SIGN_IDENTITY="" \
 CODE_SIGNING_REQUIRED=NO \
 CODE_SIGNING_ALLOWED=NO \
