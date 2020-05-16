6fb60c02c9a598184a763e61a8c134457f31df3b
Csaba Szigeti <szigetics@gmail.com>
2020-05-16 15:45:48 +0200

    Update test_app.sh

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -6,6 +6,7 @@ xcodebuild -project StravaTools.xcodeproj \
 -scheme StravaTools
 -destination platform=iOS\ Simulator,OS=13.4.1,name=iPhone\ 11 \
 CODE_SIGN_STYLE="Manual" \
+DEVELOPMENT_TEAM="" \
 CODE_SIGN_IDENTITY="" \
 CODE_SIGNING_REQUIRED=NO \
 CODE_SIGNING_ALLOWED=NO \
