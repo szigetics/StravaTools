e8a4c4ae5ce6c8ffa269d1b2bf7828e3ad76ee17
Csaba Szigeti <szigetics@gmail.com>
2020-05-24 22:40:41 +0200

    start_latlng & end_latlng

--- StravaAPIClient/Serialization/Activity.swift
+++ StravaAPIClient/Serialization/Activity.swift
@@ -8,18 +8,22 @@
 
 import Foundation
 
-struct Activity : Decodable {
+struct Activity : Codable {
     let name: String
     let distance: Float
     let max_speed: Float
     let max_heartrate: Float?
     let id: Int
+    let start_latlng: [Float]?
+    let end_latlng: [Float]?
     
-    init(name: String, distance: Float, max_speed: Float, max_heartrate: Float, id: Int) {
+    init(name: String, distance: Float, max_speed: Float, max_heartrate: Float, id: Int, start_latlng: [Float], end_latlng: [Float]) {
         self.name = name
         self.distance = distance
         self.max_speed = max_speed
         self.max_heartrate = max_heartrate
         self.id = id
+        self.start_latlng = start_latlng
+        self.end_latlng = end_latlng
     }
 }
