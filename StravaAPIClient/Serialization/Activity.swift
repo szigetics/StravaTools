28e619c485927f7999a08dd0573816b628bc48b8
Csaba Szigeti <szigetics@gmail.com>
2020-11-08 19:10:18 +0100

    Getting the start_date_local for activities

--- StravaAPIClient/Serialization/Activity.swift
+++ StravaAPIClient/Serialization/Activity.swift
@@ -17,8 +17,9 @@ struct Activity : Codable {
     let start_latlng: [Float]?
     let end_latlng: [Float]?
     let type: String
+    let start_date_local: String
     
-    init(name: String, distance: Float, max_speed: Float, max_heartrate: Float, id: Int, start_latlng: [Float], end_latlng: [Float], type: String) {
+    init(name: String, distance: Float, max_speed: Float, max_heartrate: Float, id: Int, start_latlng: [Float], end_latlng: [Float], type: String, start_date_local: String) {
         self.name = name
         self.distance = distance
         self.max_speed = max_speed
@@ -27,5 +28,14 @@ struct Activity : Codable {
         self.start_latlng = start_latlng
         self.end_latlng = end_latlng
         self.type = type
+        self.start_date_local = start_date_local
+    }
+    
+    func start_date_local_date() -> Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX") // set locale to reliable US_POSIX
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        let date = dateFormatter.date(from:start_date_local)!
+        return date
     }
 }
